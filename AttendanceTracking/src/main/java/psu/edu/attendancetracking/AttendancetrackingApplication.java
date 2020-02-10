0983ca0eb42f92f4ac12eb6b5d38bd99166d607f
sbq5018 <shannonquaill@gmail.com>
2020-02-10 14:02:23 -0500

    Initial commit

---
+++ AttendanceTracking/src/main/java/psu/edu/attendancetracking/AttendancetrackingApplication.java
@@ -0,0 +1,13 @@
+package psu.edu.attendancetracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendancetrackingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttendancetrackingApplication.class, args);
+    }
+
+}
---
+++ AttendanceTracking/src/test/java/psu/edu/attendancetracking/AttendancetrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package psu.edu.attendancetracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendancetrackingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
